d946acd528abd4f23952011372b48d68871ac911
Mike Dietz <mdietz@gmail.com>
2020-11-02 03:40:55 +0000

    Add wordcount plugin to config.

--- .eleventy.js
+++ .eleventy.js
@@ -5,11 +5,13 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const { wordCount } = require("eleventy-plugin-wordcount");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(wordCount);
 
   eleventyConfig.setDataDeepMerge(true);
 
